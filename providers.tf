762970d4724699d6044be62ea358feed48901aeb
elwync <33890511+elwync@users.noreply.github.com>
2026-06-03 14:33:43 +0800

    Change AWS region from us-west-2 to us-east-1

--- providers.tf
+++ providers.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
+  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
